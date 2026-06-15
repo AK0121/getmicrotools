a9022f324d7e78a35e24c0641c3a4fff6d476c42
AK0121 <9829859691abhishek@gmail.com>
2026-06-15 22:50:47 +0530

    website-v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mcp from 'astro-mcp';
+import tailwind from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mcp()],
+  vite: {
+    plugins: [tailwind()]
+  }
+});
