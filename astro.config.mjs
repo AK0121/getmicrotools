af9a32d78c58b2c9a207e9c3d851c59ac030a3dc
AK0121 <9829859691abhishek@gmail.com>
2026-06-20 19:12:03 +0530

    Make the webstie production ready

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,18 @@
 import { defineConfig } from 'astro/config';
 import mcp from 'astro-mcp';
 import tailwind from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
+
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mcp()],
+  site: 'https://getmicro.tools',
+  integrations: [mcp(), sitemap()],
+
   vite: {
     plugins: [tailwind()]
-  }
+  },
+
+  adapter: cloudflare()
 });
